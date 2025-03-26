eac4b10128b1730ecc44bacb5db61ec35e5e7c5b
ahijevyc <ahijevyc@ucar.edu>
2025-03-26 16:36:04 -0600

    update url in setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name="cm1",
       version="0.1",
       author="George Bryan",
       license="MIT",
-      url="https://github.com/ahijevyc/cm1",
+      url="https://github.com/NCAR/CM1",
       packages=["cm1"],
       install_requires=requirements)
