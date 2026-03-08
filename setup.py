de4f65115f9a5664583c447f159c534113a8762d
ahijevyc <ahijevyc@ucar.edu>
2026-03-08 15:45:30 -0600

    indentation

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 
 requirements = []
 
-setup(name="cm1",
-      version="0.1",
-      author="George Bryan",
-      license="MIT",
-      url="https://github.com/NCAR/CM1",
-      packages=["cm1"],
-      install_requires=requirements)
+setup(
+    name="cm1",
+    version="0.1",
+    author="George Bryan",
+    license="MIT",
+    url="https://github.com/NCAR/CM1",
+    packages=["cm1"],
+    install_requires=requirements,
+)
